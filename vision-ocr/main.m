7e087585be501ed0daa52166f6cf9d52f928829a
Peter Hosey <git@boredzo.org>
2025-02-19 20:13:09 -0800

    Initial Commit

---
+++ vision-ocr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  vision-ocr
+//
+//  Created by Peter Hosey on 2025-02-19.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
