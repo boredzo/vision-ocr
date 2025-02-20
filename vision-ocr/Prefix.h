1702e16da1f0df8efbd66893abf83a98f620f4c2
Peter Hosey <git@boredzo.org>
2025-02-20 01:05:32 -0800

    Add a prefix header.

---
+++ vision-ocr/Prefix.h
@@ -0,0 +1,17 @@
+//
+//  Prefix.h
+//  vision-ocr
+//
+//  Created by Peter Hosey on 2025-02-20.
+//
+
+#ifndef Prefix_h
+#define Prefix_h
+
+#include <stdbool.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <tgmath.h>
+#include <Foundation/Foundation.h>
+
+#endif /* Prefix_h */
